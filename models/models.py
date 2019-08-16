18a05fa09e347c2a2a36c63fb16205ad5c15fe59
nawinRoekjaroen <54171364+nawinRoekjaroen@users.noreply.github.com>
2019-08-16 16:18:25 +0700

    Update models.py

--- models/models.py
+++ models/models.py
@@ -9,11 +9,12 @@ class AcOrder(models.Model):
     name = fields.Char('Name')
     customer = fields.Many2one(comodel_name='res.partner',string='Customer')
     date = fields.Datetime('Date',required=True)
+    #สร้างตัวแปร order_line รับค่าจาก class AcOrderLine
     order_line = fields.One2many(comodel_name='ac_order_line',inverse_name='name_or',string = 'OrderLine')
 
 class AcOrderLine(models.Model):
     _name = 'ac_order_line'
-    #สร้างตัวแปร name_or เพื่อส่งค่าไป classแม่
+    #สร้างตัวแปร name_or เพื่อส่งค่าไป classAcOrder
     name_or= fields.Many2one(comodel_name='ac_order', string = 'Oderr')
     #กำหนดตัวแปร
     product = fields.Many2one(comodel_name='product.product',string='Product')
