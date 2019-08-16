70ed1d7dbc7cbecfc587965cd648f7473f8b054c
nawin <nawinboss517536@gmail.com>
2019-08-16 16:06:52 +0700

    ac_order add fields

--- models/models.py
+++ models/models.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+class AcOrder(models.Model):
+    _name = 'ac_order'
 
-# class ac_order(models.Model):
-#     _name = 'ac_order.ac_order'
+    name = fields.Char('Name')
+    customer = fields.Many2one(comodel_name='res.partner',string='Customer')
+    date = fields.Datetime('Date',required=True)
+    order_line = fields.One2many(comodel_name='ac_order_line',inverse_name='name_or',string = 'OrderLine')
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class AcOrderLine(models.Model):
+    _name = 'ac_order_line'
+     
+    name_or= fields.Many2one(comodel_name='ac_order', string = 'Oderr')
+    product = fields.Many2one(comodel_name='product.product',string='Product')
+    description = fields.Text(string='Description')
+    uom = fields.Selection([['unit','Unit'],['n','ชิ้้น'],['j','ชุด']],string='UoM')
+    unit_price = fields.Integer(string='Unit Price')
+    
