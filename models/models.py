dc18c580df3afa81fa233ed074cf649471ef401a
nawinRoekjaroen <54171364+nawinRoekjaroen@users.noreply.github.com>
2019-08-16 16:16:38 +0700

    Update models.py

--- models/models.py
+++ models/models.py
@@ -2,8 +2,10 @@
 
 from odoo import models, fields, api
 class AcOrder(models.Model):
+    #ตั้งชื่อ
     _name = 'ac_order'
-
+    
+    #กำหนดตัวแปร
     name = fields.Char('Name')
     customer = fields.Many2one(comodel_name='res.partner',string='Customer')
     date = fields.Datetime('Date',required=True)
@@ -11,8 +13,9 @@ class AcOrder(models.Model):
 
 class AcOrderLine(models.Model):
     _name = 'ac_order_line'
-     
+    #สร้างตัวแปร name_or เพื่อส่งค่าไป classแม่
     name_or= fields.Many2one(comodel_name='ac_order', string = 'Oderr')
+    #กำหนดตัวแปร
     product = fields.Many2one(comodel_name='product.product',string='Product')
     description = fields.Text(string='Description')
     uom = fields.Selection([['unit','Unit'],['n','ชิ้้น'],['j','ชุด']],string='UoM')
